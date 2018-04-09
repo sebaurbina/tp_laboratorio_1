59edc7616c7059d3de996de9a706cd4f0e1e4b4b
asus-PC\asus <sebaurbina@gmail.com>
2018-04-09 15:05:34 -0300

    TP 1 (commit 2)

--- TP_1_Cascara/funciones.c
+++ TP_1_Cascara/funciones.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float suma(float a, float b)
-{
+void menu() {
+  printf ("\nIntroduce una de las siguientes opciones:\n");
+  printf ("1.- Sumar\n");
+  printf ("2.- Restar\n");
+  printf ("3.- Multiplicar\n");
+  printf ("4.- Dividir\n");
+  printf ("5.- Factorial\n");
+  printf ("0.- Salir\n");
+  printf ("Opcion: ");
+}
+
+float suma(float a, float b) {
     float suma;
     suma = a + b;
     return suma;
 }
 
-float resta(float a, float b)
-{
+float resta(float a, float b) {
     float resta;
     resta = a - b;
     return resta;
 }
 
-float division(float a, float b)
-{
-    float division;
-    division = a / b;
-    return division;
-}
-
-float multiplicacion(float a, float b)
-{
+float multiplicar(float a, float b) {
     float multiplicacion;
     multiplicacion = a * b;
     return multiplicacion;
 }
 
+float dividir(float a, float b) {
+    float division;
+    division = a / b;
+    return division;
+}
+
+float factorial(float a) {
+    float resultado;
+    if(a==1){
+        return(1);
+    }
+    resultado=a*factorial(a-1);
+    return(resultado);
+}
--- TP_1_Cascara/funciones.h
+++ TP_1_Cascara/funciones.h
@@ -1,10 +1,6 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-
-    float suma(float a, float b); // Funcion sumar dos numeros
-    float resta(float a, float b); // Funcion restar dos numeros
-    float division(float a, float b); // Funcion division dos numeros
-    float multiplicacion(float a, float b); // Funcion multiplicacion dos numeros
-    float factorial(float a, float b); // Funcion factorial
-
-#endif // FUNCIONES_H_INCLUDED
+void menu(); // Menu de opciones
+float suma(float a, float b); // Funcion sumar dos numeros
+float resta(float a, float b); // Funcion restar dos numeros
+float multiplicar(float a, float b); // Funcion multiplicar dos numeros
+float dividir(float a, float b); // Funcion dividir dos numeros
+float factorial(float a); // Funcion para sacar factorial
